0493ffc5a7dc804853e74971bfaa3c0acd74a53a
firdausfatan <dauzztd@gmail.com>
2024-12-10 23:34:50 +0700

    Update Tampilan UI dan API.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, js}"],
+  content: [
+    "./src//*.{html,js}", // Sesuaikan dengan struktur proyekmu
+    "./src/index.html",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        // Gradien dengan posisi mulai dari 70%
+        'custom-gradient': "linear-gradient(to bottom, rgba(0, 0, 0, 0.5) 10%, rgba(0, 0, 0, 1)), url('/img/dieng1.png')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
